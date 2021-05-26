33adfc43906c55d890ad523068e7f494b0fb8959
taylor dardono <unai.zalba@gmail.com>
2021-05-26 17:10:55 +0200

    commit lab1 landing route

--- lab-01/app/src/app/app-routing.module.ts
+++ lab-01/app/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+import { LandingPageComponent } from "./landing-page/landing-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'landing', component: LandingPageComponent },
+  { path: '', redirectTo: 'landing', pathMatch: 'full' },
+  { path: '**', component: LandingPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
