855952cbae84bb533759da48821c88131677736c
Unai Zalba <unai.zalba.loperena@accenture.com>
2021-05-27 18:31:49 +0200

    update angular 11 task01

--- openathon_day/task01-ReactiveForms/lab-04/app/src/app/events/event-list/event-list.component.spec.ts
+++ openathon_day/task01-ReactiveForms/lab-04/app/src/app/events/event-list/event-list.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { EventListComponent } from './event-list.component';
 
@@ -6,7 +6,7 @@ describe('EventListComponent', () => {
   let component: EventListComponent;
   let fixture: ComponentFixture<EventListComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ EventListComponent ]
     })
